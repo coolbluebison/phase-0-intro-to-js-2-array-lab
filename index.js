8194086da2e456106472f66db4c7bcd96c0778a9
root <+>
2023-07-10 21:46:02 -0600

    itswhatitis

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+};
+
+
+
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+};
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+
+function removeLastCat() {
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
+
+
+
+
+
+
+
+
+
+
